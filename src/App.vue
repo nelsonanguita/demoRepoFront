9fa74366cb690357798b0ac7712b025661d548e5
alexis bustamante <alexisbustamantecisternas@gmail.com>
2022-03-09 16:18:39 -0300

    main

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,76 @@
 <template>
-  <v-app>
-    <v-app-bar app  color="teal"
-          dark>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-      <v-toolbar-title>MENU</v-toolbar-title>
+<v-app>
 
-    
-    </v-app-bar>
- 
-     <v-navigation-drawer 
-     app 
-     v-model="drawer" 
-     temporary 
-     
-    >
-    <v-list-item @click.stop="drawer = !drawer">
+    <v-app-bar app color="teal" dark absolute :clippedLeft="true" elevation-24 :flat="true">
         <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        <v-list-item-content >
-          <v-list-item-title class="text-h6 text-center"  >
-            MENU
-          </v-list-item-title>
-          <v-list-item-subtitle>
-            
-          </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
+        <v-toolbar-title></v-toolbar-title>
+        <v-spacer>
 
-      <v-divider></v-divider>
+        </v-spacer>
 
-       <v-list
-          nav
-          dense
-        >
-          <v-list-item 
-          v-for="item in items" :key="item.title" link :to="item.path"
-          >
-            <v-list-item-icon>
-              <v-icon>{{item.icon}}</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title >{{item.name}}</v-list-item-title>
-          </v-list-item>
+        <v-btn color="primary" @click="showLogin=true"> Login
+
+        </v-btn>
+
+    </v-app-bar>
+    <v-navigation-drawer app permanent>
+        <v-list-item @click.stop="drawer = !drawer">
+            <v-app-bar-nav-icon @click.stop="drawer = !drawer">
+            </v-app-bar-nav-icon>
+            <v-list-item-content>
+                <v-list-item-title class="text-h6 text-center">
+                </v-list-item-title> menu
+                <v-list-item-subtitle>
+                </v-list-item-subtitle>
+            </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+        <v-list nav dense>
+            <v-list-item v-for="item in items" :key="item.title" link :to="item.path">
+                <v-list-item-icon>
+                    <v-icon>{{item.icon}}</v-icon>
+                </v-list-item-icon>
+                <v-list-item-title>{{item.name}}</v-list-item-title>
+            </v-list-item>
         </v-list>
-      </v-navigation-drawer>
+    </v-navigation-drawer>
 
     <v-main>
-      <router-view />
+        <v-container fluid>
+            <router-view></router-view>
+        </v-container>
     </v-main>
-  </v-app>
+</v-app>
 </template>
 
 <script>
 export default {
-  name: "App",
+    name: "App",
+    components: {
 
-  data: () => ({
-    items:[
-      {
-      path:'/',
-      name:'Home',
-      icon:'mdi-home'
     },
-    {
-       path:'/ListadoMetodos',
-      name:'Documentación',
-      icon:'mdi-folder'
-    },
-        {
-       path:'/about',
-      name:'Nosotros',
-      icon:'mdi-car'
-    }
-    ],
-    drawer: false,
-    //
-  }),
+    data: () => ({
+        showLogin: false,
+        drawer: true,
+        items: [{
+                path: '/',
+                name: 'Home',
+                icon: 'mdi-home'
+            },
+            {
+                path: '/ListadoMetodos',
+                name: 'Documentación',
+                icon: 'mdi-folder'
+            },
+            {
+                path: '/about',
+                name: 'Nosotros',
+                icon: 'mdi-car'
+            }
+        ],
+        //
+    }),
+
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,13 @@
 <template>
-
-  <div class="ma-12 pa-12">
-
-        <hello-world />
-
-  </div>
+  
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+export default {
 
-  export default {
-    name: 'Home',
-
-    components: {
-      HelloWorld,
-    },
-  }
+}
 </script>
+
+<style>
+
+</style>
